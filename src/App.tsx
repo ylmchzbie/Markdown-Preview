df9c64cf3b14c299b4fe0487ad401f2be8571414
Caryl Manalo <marcbmanalo@addu.edu.ph>
2025-05-29 04:44:01 +0800

    functioning markdown previewer, no ui design

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
 import { useState } from 'react'
+import { marked } from 'marked'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Configure marked to NOT break on single line breaks
+marked.setOptions({
+  breaks: false, // Only double line breaks create new paragraphs
+})
+
+const defaultMarkdown = `# Heading 1
+
+## Heading 2
+
+[GitHub](https://github.com/ylmchzbie)
+
+Here is some \`inline code\`.
+
+\`\`\`js
+// Code block
+function greet() {
+  console.log("Hello, world!");
+}
+\`\`\`
+
+- List item 1
+- List item 2
+
+> This is a blockquote.
+
+![React Logo](https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg)
+
+**Bolded text**
+`
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setMarkdown] = useState(defaultMarkdown)
 
   return (
     <>
       <div>
-        <textarea id='editor'></textarea>
+        <textarea
+          id='editor'
+          value={markdown}
+          onChange={e => setMarkdown(e.target.value)}
+        ></textarea>
       </div>
-      <div id='preview'>
-
-      </div>
+      <div
+        id='preview'
+        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+      />
     </>
   )
 }
